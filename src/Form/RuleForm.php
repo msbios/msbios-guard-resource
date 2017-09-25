eb3a4c8e4f5b0635e927da1a98c2006ec3589a63
Judzhin Miles <info@msbios.com>
2017-09-25 16:52:30 +0300

    Lazy Composer Commiter

---
+++ src/Form/RuleForm.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Guard\Resource\Form;
+
+use MSBios\Resource\Initializer\LazyFormAwareInterface;
+use Zend\Form\Element\Text;
+use Zend\Form\Form;
+
+/**
+ * Class RuleForm
+ * @package MSBios\Guard\Resource\Form
+ */
+class RuleForm extends Form implements LazyFormAwareInterface
+{
+    public function init()
+    {
+        $this->add([
+            'type' => Text::class,
+            'name' => 'name'
+        ])->add([
+            'type' => Text::class,
+            'name' => 'access'
+        ]);
+    }
+}
